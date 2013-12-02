3fa2f231113a5dd98703cfb540f4c29be15f63c7
Saurav Majumder <saurav.majumder@ufl.edu>
2013-12-02 17:28:25 -0500

    Create unknownGestureFragementViewTask.java

---
+++ unknownGestureFragementViewTask.java
@@ -0,0 +1,45 @@
+package com.example.remindme;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class unknownGestureFragementViewTask extends DialogFragment{
+	public interface NoticeDialogListener {
+        public void onSelectItem(int position);
+    }
+
+    NoticeDialogListener mListener;
+    
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Activity MainActivity) {
+        super.onAttach(MainActivity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (NoticeDialogListener) MainActivity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(MainActivity.toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+    
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+	    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+	    builder.setTitle(R.string.uknown_gesture);
+	    builder.setItems(R.array.unkownTaskView, new DialogInterface.OnClickListener() {
+	               public void onClick(DialogInterface dialog, int which) {
+	               // The 'which' argument contains the index position
+	               // of the selected item
+	            	   mListener.onSelectItem(which);
+	           }
+	    });
+	    return builder.create();
+	}
+}
